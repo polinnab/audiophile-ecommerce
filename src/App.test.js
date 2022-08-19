760973b20720ed1c9272645a760c9d0d9c3427c6
polinnab <p.tsunenko94@gmail.com>
2022-08-19 13:18:28 +0300

    add @babel/plugin-syntax-jsx for rendering jsx files in tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import {render, screen} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Jest text');
-  expect(linkElement).toBeInTheDocument();
+test('renders', () => {
+  const {container} = render(<App />);
+  expect(container).toMatchInlineSnapshot(`
+    <div>
+      <div>
+        Jest text
+      </div>
+    </div>
+  `);
 });

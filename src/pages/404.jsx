b11402fdc625f435a899b762123598a7aab3ff1d
polinnab <p.tsunenko94@gmail.com>
2022-08-22 17:04:55 +0300

    update 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Page404() {
   return (
     <>
       <h1>404</h1>
       <br>Page not found</br>
-      <br>Go to Homepage</br>
+      <br>
+        Go to <Link to="/">Homepage</Link>
+      </br>
     </>
   );
 }

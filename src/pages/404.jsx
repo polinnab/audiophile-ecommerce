1759db69e59db0a99cf1c7ca1361c46c578851db
polinnab <p.tsunenko94@gmail.com>
2022-08-22 17:02:42 +0300

    create 2 pages

---
+++ src/pages/404.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Page404() {
+  return (
+    <>
+      <h1>404</h1>
+      <br>Page not found</br>
+      <br>Go to Homepage</br>
+    </>
+  );
+}
+
+export default Page404;

e1dfa22200d8bd767e19d41c2e24532a37311fb4
polinnab <p.tsunenko94@gmail.com>
2022-08-23 15:05:28 +0300

    check workings fonts

---
+++ src/design-system/typography/typography.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const PrimaryLarge = styled.h1`
+  font-family: 'Manrope', sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 56px;
+  line-height: 58px;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+`;
+
+export {PrimaryLarge as H1};
+// export { PrimaryMedium as H2 };
+// export { PrimarySmall as H3 };
+// export { SecondaryLarge as H4 };
+// export { SecondaryMedium as H5 };
+// export { SecondarySmall as H6 };

cc87eedbe5d6ab52ce372ad52a6f24bf9bc8db17
polinnab <p.tsunenko94@gmail.com>
2022-08-23 11:44:57 +0300

    add colors to design system

---
+++ src/design-system/colors/colors.js
@@ -0,0 +1,10 @@
+export const OCHRE = '#D87D4A';
+export const OCHRE_LIGHT = '#FBAF85';
+
+export const BLACK = '#000000';
+export const DARK = '#101010';
+
+export const GRAY = '#F1F1F1';
+export const GRAY_LIGHT = '#FAFAFA';
+
+export const WHITE = '#FFFFFF';
